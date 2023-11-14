f1dda6527c4ae5a3c6427464ad13b1194874e2f3
joel2982 <jjoel9958@gmail.com>
2023-11-14 12:08:30 +0530

    Cleaned code

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 import os
-import pickle
-import openai
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import faiss
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
-import mysql.connector
 import MySQLdb
 from langchain.chat_models import ChatOpenAI
 from chatui import css,user_template,bot_template
@@ -42,67 +39,37 @@ def new_session_state():
         st.session_state.current_chat = None
         st.session_state.conversation = None
         st.session_state.chat_history = None
-        st.session_state.embeddings = None
         st.session_state.title = None
 
-def get_pdf_text(pdf_docs):
-    title = []
-    text_docs = []
-    for pdf in pdf_docs:
-        title.append(pdf.name[:-4])
-        pdf_reader = PdfReader(pdf)
-        text = ''
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-        text_docs.append(text)
-    return title,text_docs
+def get_pdf_text(pdf):
+    title = pdf.name[:-4]
+    pdf_reader = PdfReader(pdf)
+    text = ''
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return title,text
 
-def get_text_chunks(texts):
+def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = []
-    for text in texts:
-        chunks.append(text_splitter.split_text(text))
+    chunks = text_splitter.split_text(text)
     return chunks
 
-def get_vectorstore(titles,text_chunks):
-    it_titles = iter(titles)
-    title = next(it_titles)
-    # if os.path.exists(f'embeddings\{title}.pkl'):
-    #     with open(f'embeddings\{title}.pkl','rb') as f:
-    #         vectorstore = pickle.load(f)
-    # else:
-    index = titles.index(title)
+def get_vectorstore(title,text_chunk):
     embeddings = OpenAIEmbeddings()
-    vectorstore = faiss.FAISS.from_texts(texts=text_chunks[index],embedding=embeddings)
-        # with open(f'embeddings\{title}.pkl','wb') as f:
-        #     pickle.dump(vectorstore,f)
-    for title in it_titles:
-        #dv = pd.Dataframe()
-        if os.path.exists(f'\embeddings\{title}.pkl'):
-            with open(f'\embeddings\{title}.pkl','rb') as f:
-                dv = pickle.load(f)
-        else:
-            index = titles.index(title)
-            embeddings = OpenAIEmbeddings()
-            dv = faiss.FAISS.from_texts(texts=text_chunks[index],embedding=embeddings)
-            with open(f'embeddings\{title}.pkl','wb') as f:
-                pickle.dump(dv,f)
-        vectorstore.merge_from(dv)
+    vectorstore = faiss.FAISS.from_texts(texts=text_chunk,embedding=embeddings)
     return vectorstore
 
-def get_conversation_chain():
+def get_conversation_chain(embeddings):
     llm = ChatOpenAI(temperature=0.0, model_name = "gpt-3.5-turbo")
-    retriever = st.session_state.embeddings.as_retriever()
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation = ConversationalRetrievalChain.from_llm(
         llm = llm,
-        chain_type = "stuff",
         memory = memory,
-        retriever = retriever
+        retriever = embeddings.as_retriever()
     )
     return conversation
 
@@ -115,39 +82,6 @@ def delete_chat():
     st.session_state.clear()
     new_session_state()
     st.empty()
-
-
-def chat_output():
-    try:        
-        for i,chat in enumerate(st.session_state.chat_history):
-            if i%2 == 0:
-                st.write(user_template.replace("{{MSG}}",chat.content), unsafe_allow_html=True)
-            else:
-                st.write(bot_template.replace("{{MSG}}",chat.content), unsafe_allow_html=True)
-    except TypeError:
-        pass
-
-def handle_user_input(user_question):
-    response = st.session_state.conversation(user_question)
-    st.session_state.chat_history = response['chat_history']
-    chat_history = str(st.session_state.chat_history)
-    if len(st.session_state.chat_history) == 2:
-        title = st.session_state.chat_history[0].content
-        sql_cmd = "INSERT INTO chat_history (title, vs_name, chat_history) VALUES (%s, %s, %s)"
-        values = (title,st.session_state.title,chat_history)
-    else:
-        sql_cmd = "UPDATE chat_history SET chat_history = %s WHERE id = %s"
-        values = (chat_history,st.session_state.current_chat)
-    dml_cursor = chatdb.cursor()
-    dml_cursor.execute(sql_cmd,values)
-    chatdb.commit()
-
-    if not st.session_state.current_chat:
-        select_cursor = chatdb.cursor()
-        sql_cmd = f"SELECT id FROM chat_history WHERE title = '{title}'"
-        select_cursor.execute(sql_cmd)
-        st.session_state.current_chat = select_cursor.fetchone()
-
 
 def retrieve_names(cmd):
     string = list(str(cmd.fetchall()))
@@ -184,7 +118,43 @@ def retrieve_messages(cmd):
     chats.append(AIMessage(content=messages[i+1]))
     return chats
 
+def chat_output():
+    try:        
+        for i,chat in enumerate(st.session_state.chat_history):
+            if i%2 == 0:
+                st.write(user_template.replace("{{MSG}}",chat.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace("{{MSG}}",chat.content), unsafe_allow_html=True)
+    except TypeError:
+        pass
+
+def handle_user_input(user_question):
+    response = st.session_state.conversation(user_question)
+    st.session_state.chat_history = response['chat_history']
+    chat_history = str(st.session_state.chat_history)
+    # Initializing a new chat in the database table 
+    if len(st.session_state.chat_history) == 2:
+        title = st.session_state.chat_history[0].content
+        sql_cmd = "INSERT INTO chat_history (title, vs_name, chat_history) VALUES (%s, %s, %s)"
+        values = (title,st.session_state.title,chat_history)
+    # Updating the current chat in the database table 
+    else:
+        sql_cmd = "UPDATE chat_history SET chat_history = %s WHERE id = %s"
+        values = (chat_history,st.session_state.current_chat)
+    dml_cursor = chatdb.cursor()
+    dml_cursor.execute(sql_cmd,values)
+    chatdb.commit()
+    # Getting the current chat id 
+    if not st.session_state.current_chat:
+        select_cursor = chatdb.cursor()
+        sql_cmd = f"SELECT id FROM chat_history WHERE title = '{title}'"
+        select_cursor.execute(sql_cmd)
+        st.session_state.current_chat = select_cursor.fetchone()
+
+
+
 def previous_chat_loader(chat):
+    # retrieving chat id, chat history and the pdf name
     select_cursor = chatdb.cursor()
     sql_cmd = f"SELECT id,chat_history FROM chat_history WHERE title = '{chat}'"
     select_cursor.execute(sql_cmd)
@@ -224,20 +194,20 @@ def main():
         add_vertical_space(2)
         # PDF Uploader
         with st.form("my-form", clear_on_submit=True):
-            pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
+            pdf = st.file_uploader("Upload your PDF Files")
             submitted = st.form_submit_button("UPLOAD")
         # PDF -> vectorstore
-        if submitted and len(pdf_docs) != 0:
+        if submitted and pdf:
             with st.spinner('Processing'):
                 #get the pdf text
-                title,text_docs = get_pdf_text(pdf_docs)
-                st.session_state.title = title[0]
+                title,text = get_pdf_text(pdf)
+                st.session_state.title = title
                 #divide the pdf text into text chunks
-                text_chunks = get_text_chunks(text_docs)
+                text_chunk = get_text_chunks(text)
                 #create vectorstores
-                st.session_state.embeddings = get_vectorstore(title,text_chunks)
+                embeddings = get_vectorstore(title,text_chunk)
                 #initializing conversation
-                st.session_state.conversation = get_conversation_chain()
+                st.session_state.conversation = get_conversation_chain(embeddings)
         add_vertical_space(4)
         st.write('Made with by [Joel John](https://www.linkedin.com/in/joeljohn29082002/)')
 
@@ -251,6 +221,7 @@ def main():
             delete_chat()
         except:
             st.warning(" Please Select the Chat to be deleted. ")
+            st.stop()
     if del_chat:
         st.rerun()
 
