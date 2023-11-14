230c17e8cb633e936e82dc30f31cd88cfb298fd8
joel2982 <jjoel9958@gmail.com>
2023-11-14 10:45:36 +0530

    Integrated with server

--- app.py
+++ app.py
@@ -14,12 +14,20 @@ import MySQLdb
 from langchain.chat_models import ChatOpenAI
 from chatui import css,user_template,bot_template
 from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.schema.messages import HumanMessage, AIMessage
 
-#OPENAI_API_KEY=st.secrets['OPENAI_API_KEY']
-host = os.getenv("host")
-user = os.getenv("user")
-password = os.getenv("password")
-database = os.getenv("database")
+OPENAI_API_KEY=st.secrets['OPENAI_API_KEY']
+host = st.secrets("HOST")
+user = st.secrets("USER")
+password = st.secrets("PASSWORD")
+database = st.secrets("DATABASE")
+
+# load_dotenv()
+# host = os.getenv("HOST")
+# user = os.getenv("USER")
+# password = os.getenv("PASSWORD")
+# database = os.getenv("DATABASE")
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 config = {
     'host':host,
@@ -30,9 +38,12 @@ config = {
 chatdb = MySQLdb.Connect(**config)
 
 def new_session_state():
-    st.session_state.conversation = None
-    st.session_state.chat_history = None
-    st.session_state.chain = None
+    if 'current_chat' not in st.session_state:
+        st.session_state.current_chat = None
+        st.session_state.conversation = None
+        st.session_state.chat_history = None
+        st.session_state.embeddings = None
+        st.session_state.title = None
 
 def get_pdf_text(pdf_docs):
     title = []
@@ -95,106 +106,164 @@ def get_conversation_chain():
     )
     return conversation
 
-def create_chat_history():
-    st.write("creating")
-    create_cursor = chatdb.cursor()
-    sql_cmd = "INSERT INTO chat_history (title, vs_name, chat_history) VALUES (%s, %s, %s)"
-    chat_history = str(st.session_state.chat_history)
-    values = ("joel",st.session_state.title,chat_history)
-    create_cursor.execute(sql_cmd,values)
+def delete_chat():
+    sql_cmd = "DELETE FROM chat_history WHERE id = %s"
+    values = str(st.session_state.current_chat)
+    dml_cursor = chatdb.cursor()
+    dml_cursor.execute(sql_cmd,values)
     chatdb.commit()
-    st.write(create_cursor.rowcount, "record(s) affected")
+    st.session_state.clear()
+    new_session_state()
+    st.empty()
 
 
-def update_chat_history():
-    st.write("updating")
-    update_cursor = chatdb.cursor()
-    sql_cmd = "UPDATE chat_history SET chat_history = %s WHERE id = %s"
-    chat_history = str(st.session_state.chat_history)
-    values = (chat_history,1)
-    update_cursor.execute(sql_cmd,values)
-    chatdb.commit()
-    st.write(update_cursor.rowcount, "record(s) affected")
-
+def chat_output():
+    try:        
+        for i,chat in enumerate(st.session_state.chat_history):
+            if i%2 == 0:
+                st.write(user_template.replace("{{MSG}}",chat.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace("{{MSG}}",chat.content), unsafe_allow_html=True)
+    except TypeError:
+        pass
 
 def handle_user_input(user_question):
     response = st.session_state.conversation(user_question)
     st.session_state.chat_history = response['chat_history']
-    st.write(str(st.session_state.chat_history))
-    for i,chat in enumerate(st.session_state.chat_history):
-        if i%2 == 0:
-            st.write(user_template.replace("{{MSG}}",chat.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace("{{MSG}}",chat.content), unsafe_allow_html=True)
+    chat_history = str(st.session_state.chat_history)
     if len(st.session_state.chat_history) == 2:
-        create_chat_history()
+        title = st.session_state.chat_history[0].content
+        sql_cmd = "INSERT INTO chat_history (title, vs_name, chat_history) VALUES (%s, %s, %s)"
+        values = (title,st.session_state.title,chat_history)
     else:
-        update_chat_history()
+        sql_cmd = "UPDATE chat_history SET chat_history = %s WHERE id = %s"
+        values = (chat_history,st.session_state.current_chat)
+    dml_cursor = chatdb.cursor()
+    dml_cursor.execute(sql_cmd,values)
+    chatdb.commit()
+
+    if not st.session_state.current_chat:
+        select_cursor = chatdb.cursor()
+        sql_cmd = f"SELECT id FROM chat_history WHERE title = '{title}'"
+        select_cursor.execute(sql_cmd)
+        st.session_state.current_chat = select_cursor.fetchone()
+
 
 def retrieve_names(cmd):
     string = list(str(cmd.fetchall()))
     names=[]
     val = ''
     for i in string:
-        if i in ',':
+        if i in ' ,':
             if len(val)==0:
+                continue
+            elif i in ' ':
+                val+=i
                 continue
             names.append(val)
             val = ''
-        elif i not in " ''[]()":
+        elif i not in "''[]()":
             val+=i
-    names.sort()
     return names
+
+def retrieve_messages(cmd):
+    messages = list(cmd.fetchone()[1].replace('), H','), A').split('), A'))
+    chats = []
+    messages[0] = messages[0].replace('[H','')
+    for i in range(0,len(messages)-1):
+        if i%2 == 0:
+            messages[i] = messages[i].replace('umanMessage(content=\'','')
+            messages[i] = messages[i][0:-1]
+            chats.append(HumanMessage(content=messages[i]))
+            
+        else:
+            messages[i] = messages[i].replace('IMessage(content=\'','')
+            messages[i] = messages[i][0:-1]
+            chats.append(AIMessage(content=messages[i]))
+    messages[i+1] = messages[i+1][18:-3]
+    chats.append(AIMessage(content=messages[i+1]))
+    return chats
+
+def previous_chat_loader(chat):
+    select_cursor = chatdb.cursor()
+    sql_cmd = f"SELECT id,chat_history FROM chat_history WHERE title = '{chat}'"
+    select_cursor.execute(sql_cmd)
+    st.session_state.current_chat = select_cursor.fetchone()[0]
+    select_cursor.execute(sql_cmd)  
+    st.session_state.chat_history = retrieve_messages(select_cursor)
+    sql_cmd = f"SELECT vs_name FROM chat_history WHERE title = '{chat}'"
+    select_cursor.execute(sql_cmd)  
+    st.session_state.title = select_cursor.fetchone()[0]
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title='Chat with PDFs',page_icon='books:')
     st.write(css, unsafe_allow_html=True)
-    if 'conversation' not in st.session_state:
-        st.session_state.conversation = None
-    if 'chat_history' not in st.session_state:
-        st.session_state.chat_history = None
-    if 'embeddings' not in st.session_state:
-        st.session_state.embeddings = None
-    if 'title' not in st.session_state:
-        st.session_state.title = None
     st.subheader('Chat with your PDFs :books:')
-    st.write(password,host,user,database)
     select_cursor = chatdb.cursor()
     select_cursor.execute("SELECT title FROM chat_history")
     chat_titles = retrieve_names(select_cursor)
+    new_session_state()
     with st.sidebar:
-        if st.button('New Chat'):
-            st.session_state.clear()
-            new_session_state()
+        col1, col2 = st.columns(2)
+        with col1:
+            new_chat = st.button('New Chat',use_container_width=True)
+        with col2:
+            del_chat = st.button('Delete Chat',use_container_width=True)
+
         add_vertical_space(2)
-        chat = st.selectbox(label="Previous Chats",options=chat_titles, index=None, placeholder="Select the Chat")
+        # Previous Chats
+        st.subheader("Previous Chats")
+        for chat in chat_titles:
+            if st.sidebar.button(chat, key=chat, use_container_width=True):
+                st.session_state.clear()
+                new_session_state()
+                st.empty()
+                previous_chat_loader(chat)
         add_vertical_space(2)
+        # PDF Uploader
         with st.form("my-form", clear_on_submit=True):
             pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
             submitted = st.form_submit_button("UPLOAD")
+        # PDF -> vectorstore
         if submitted and len(pdf_docs) != 0:
             with st.spinner('Processing'):
                 #get the pdf text
                 title,text_docs = get_pdf_text(pdf_docs)
-                st.session_state.title = title 
+                st.session_state.title = title[0]
                 #divide the pdf text into text chunks
                 text_chunks = get_text_chunks(text_docs)
                 #create vectorstores
                 st.session_state.embeddings = get_vectorstore(title,text_chunks)
                 #initializing conversation
                 st.session_state.conversation = get_conversation_chain()
-        elif chat:
-            sql_cmd = f"SELECT id FROM chat_history WHERE title = '{chat}'"
-            select_cursor.execute(sql_cmd)
-            st.session_state.chat_id = select_cursor.fetchone()[0]
         add_vertical_space(4)
-        st.write('Made with ❤️ by [Joel John](https://www.linkedin.com/in/joeljohn29082002/)')
+        st.write('Made with by [Joel John](https://www.linkedin.com/in/joeljohn29082002/)')
+
+    if new_chat:
+        st.session_state.clear()
+        new_session_state()
+        st.empty()
+        st.success(" Please upload the PDF")
+    if del_chat:
+        try:
+            delete_chat()
+        except:
+            st.warning(" Please Select the Chat to be deleted. ")
+    if del_chat:
+        st.rerun()
+
     if st.session_state.conversation:
+        st.success(f"PDF Used: {st.session_state.title}")
         user_question = st.text_input('Ask your Questions!')
-        st.write(bot_template.replace("{{MSG}}","How can I help you!"), unsafe_allow_html=True)
         if user_question:
             handle_user_input(user_question)
+            chat_output()
+    elif st.session_state.chat_history:
+        st.success(f"PDF Used: {st.session_state.title}")
+        chat_output()
+    chatdb.close() 
 
 if __name__ == '__main__':
     main()
